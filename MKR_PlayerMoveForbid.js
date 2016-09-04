6c2f85e87608fd01509cfdf63382810e7d2300a7
mankind <mkr.tk.games@gmail.com>
2016-09-04 20:03:10 +0900

    ver 1.0.1 : デフォルト値が間違っていたため修正

--- MKR_PlayerMoveForbid.js
+++ MKR_PlayerMoveForbid.js
@@ -6,7 +6,10 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
-// 1.0.0 2016/09/04 初版公開
+// 1.0.1 2016/09/04 未使用のコードを削除しファイル容量を小さくした。
+//                  デフォルト値の設定が不適切だったので修正。
+//
+// 1.0.0 2016/09/04 初版公開。
 // ----------------------------------------------------------------------------
 // [Twitter] https://twitter.com/mankind_games/
 //  [GitHub] https://github.com/mankindGames/
@@ -14,11 +17,14 @@
 
 /*:
  *
- * @plugindesc (v1.0.0) 指定された番号のスイッチがONの間、
+ * @plugindesc (v1.0.1) 指定された番号のスイッチがONの間、
  * プレイヤー操作によるキャラの移動を禁止します。
+ *
  * @author mankind
  *
  * @help
+ * 指定された番号のスイッチがONの間、
+ * プレイヤー操作によるキャラの移動を禁止します。
  *
  *
  * 簡単な使い方説明:
@@ -34,26 +40,6 @@
  *
  * スクリプトコマンド:
  *   ありません。
- *
- *
- * 補足：
- *   ・このプラグインに関するメモ欄の設定、プラグインコマンド、
- *     制御文字は大文字/小文字を区別していません。
- *
- *   ・プラグインパラメーターの説明に、[初期値]と書かれているものは
- *     アクター/イベントなどのメモ欄で個別に設定が可能です。
- *     設定した場合、[初期値]よりメモ欄の設定が
- *     優先されますのでご注意ください。
- *
- *   ・プラグインパラメーターの説明に、[変数可]と書かれているものは
- *     設定値に変数を表す制御文字である\v[n]を使用可能です。
- *     変数を設定した場合、そのパラメーターの利用時に変数の値を
- *     参照するため、パラメーターの設定をゲーム中に変更できます。
- *
- *   ・プラグインパラメーターの説明に、[スイッチ可]と書かれているものは
- *     設定値にスイッチを表す制御文字である\s[n]を使用可能です。
- *     スイッチを設定した場合、そのパラメーターの利用時にスイッチの値を
- *     参照するため、パラメーターの設定をゲーム中に変更できます。
  *
  *
  * 利用規約:
@@ -106,29 +92,12 @@
 
         if(!regExp.test(value)) {
             switch(type) {
-                case "bool":
-                    if(value == "") {
-                        value = (def)? true : false;
-                    }
-                    value = value.toUpperCase() === "ON" || value.toUpperCase() === "TRUE" || value.toUpperCase() === "1";
-                    break;
                 case "num":
                     if(value == "") {
                         value = (isFinite(def))? parseInt(def, 10) : 0;
                     } else {
                         value = (isFinite(value))? parseInt(value, 10) : (isFinite(def))? parseInt(def, 10) : 0;
                         value = value.clamp(min, max);
-                    }
-                    break;
-                case "string":
-                    value = value;
-                    break;
-                case "switch":
-                    if(value == "") {
-                        value = (def != "")? def : value;
-                    }
-                    if(!value.match(/^([A-D]|\d+)$/i)) {
-                        throw new Error('Plugin parameter value is not switch : '+param+' : '+value);
                     }
                     break;
                 default:
@@ -140,102 +109,8 @@
         return [value, type, def, min, max];
     }
 
-    var CEC = function(params) {
-        var text, value, type, def, min, max;
-        type = params[1];
-        text = String(params[0]);
-        text = text.replace(/\\/g, '\x1b');
-        text = text.replace(/\x1b\x1b/g, '\\');
-        type = params[1];
-        def = params[2];
-        min = params[3];
-        max = params[4];
-
-        text = String(ConvVb(text));
-        text = String(ConvSw(text));
-
-        switch(type) {
-            case "bool":
-                if(text == "") {
-                    value = (def)? true : false;
-                } else {
-                    value = text.toUpperCase() === "ON" || text.toUpperCase() === "TRUE" || text.toUpperCase() === "1";
-                }
-                break;
-            case "num":
-                value = (isFinite(text))? parseInt(text, 10) : (isFinite(def))? parseInt(def, 10) : 0;
-                value = value.clamp(min, max);
-                break;
-            case "string":
-                if(text == "") {
-                    value = (def != "")? def : value;
-                } else {
-                    value = text;
-                }
-                break;
-            case "switch":
-                if(value == "") {
-                    value = (def != "")? def : value;
-                }
-                if(!value.match(/^([A-D]|\d+)$/)) {
-                    throw new Error('Plugin parameter value is not switch : '+param+' : '+value);
-                }
-                break;
-            default:
-                throw new Error('[CEC] Plugin parameter type is illegal: '+type);
-                break;
-        }
-
-        return value;
-    };
-
-    var ConvVb = function(text) {
-        var num;
-
-        if(typeof text == "string") {
-            text = text.replace(/\x1bV\[(\d+)\]/i, function() {
-                num = parseInt(arguments[1]);
-                return $gameVariables.value(num);
-            }.bind(this));
-            text = text.replace(/\x1bV\[(\d+)\]/i, function() {
-                num = parseInt(arguments[1]);
-                return $gameVariables.value(num);
-            }.bind(this));
-        }
-
-        return text;
-    }
-
-    var ConvSw = function(text, target) {
-        var num, key;
-
-        if(typeof text == "string") {
-            text = text.replace(/\x1bS\[(\d+)\]/i, function() {
-                num = parseInt(arguments[1]);
-                return $gameSwitches.value(num);
-            }.bind(this));
-            text = text.replace(/\x1bS\[([A-D])\]/i, function() {
-                if(target) {
-                    key = [target._mapId, target._eventId, arguments[1].toUpperCase()];
-                    return $gameSelfSwitches.value(key);
-                }
-                return false;
-            }.bind(this));
-
-
-            if(text === true || text.toLowerCase() === "true") {
-                text = 1;
-            } else {
-                text = 0;
-            }
-
-        }
-
-        return text;
-    }
-
     var DefMoveFlag;
-    DefMoveFlag = CheckParam("num", "Default_Move_Flag", false);
+    DefMoveFlag = CheckParam("num", "Default_Move_Flag", 10);
 
 
     //=========================================================================
