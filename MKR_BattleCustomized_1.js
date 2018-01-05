d70f6c20ddd5045fd2cd20b58230383129a66c18
mankindGames <mkr.tk.games@gmail.com>
2018-01-05 15:55:05 +0900

    ver 1.0.1 : アクター行動選択中以外の行動でも前進距離が変化していたため、アクター行動選択中のみ前進しなくなるよう修正。

--- MKR_BattleCustomized_1.js
+++ MKR_BattleCustomized_1.js
@@ -6,6 +6,8 @@
 // http://opensource.org/licenses/mit-license.php
 // ------------------------------------------------------------------------------
 // Version
+// 1.0.1 2017/01/05 アクター選択中以外の行動時にも前進しなくなっていたため修正。
+//
 // 1.0.0 2017/01/05 初版公開。
 // ------------------------------------------------------------------------------
 // [Twitter] https://twitter.com/mankind_games/
@@ -15,15 +17,27 @@
 
 /*:
  * ==============================================================================
- * @plugindesc (v1.0.0) バトルシーンカスタマイズプラグインその1
+ * @plugindesc (v1.0.1) バトルシーンカスタマイズプラグインその1
  * @author マンカインド
  *
  * @help このプラグインを導入することでバトルシーンに以下の変更を行います。
  *
- *   ・アクターが行動選択状態のとき、アクターの前進距離をピクセル単位で調整可能。
+ *   ・アクターが行動選択状態のとき、そのアクターが前進しなくなる。
  *
  *
  * その1 と名付けていますがこのプラグイン単体で動作します。
+ *
+ *
+ * プラグインパラメータ:
+ *   ありません。
+ *
+ *
+ * プラグインコマンド:
+ *   ありません。
+ *
+ *
+ * スクリプトコマンド:
+ *   ありません。
  *
  *
  * 利用規約:
@@ -42,15 +56,6 @@
  *     ご了承ください。
  *
  * ==============================================================================
- *
- * @param actor_step
- * @text アクター移動距離
- * @desc アクター行動選択状態の移動距離をピクセル単位で指定します。(デフォルト:48)
- * @type number
- * @min 0
- * @max 9999
- * @default 48
- *
 */
 
 var Imported = Imported || {};
@@ -59,62 +64,25 @@ Imported.MKR_BattleCustomized_1 = true;
 (function () {
     'use strict';
 
-    const PN = "MKR_BattleCustomized_1";
-
-    const CheckParam = function(type, param, def, min, max) {
-        let Parameters, regExp, value;
-        Parameters = PluginManager.parameters(PN);
-
-        if(arguments.length < 4) {
-            min = -Infinity;
-            max = Infinity;
-        }
-        if(arguments.length < 5) {
-            max = Infinity;
-        }
-        if(param in Parameters) {
-            value = String(Parameters[param]);
-        } else {
-            throw new Error("[CheckParam] プラグインパラメーターがありません: " + param);
-        }
-
-        value = value.replace(/\\/g, '\x1b');
-        value = value.replace(/\x1b\x1b/g, '\\');
-
-        regExp = /(\x1bV|\x1bN)\[\d+\]/i;
-        if(!regExp.test(value)) {
-            switch(type) {
-                case "num":
-                    if(value == "") {
-                        value = (isFinite(def))? parseInt(def, 10) : 0;
-                    } else {
-                        value = (isFinite(value))? parseInt(value, 10) : (isFinite(def))? parseInt(def, 10) : 0;
-                        value = value.clamp(min, max);
-                    }
-                    break;
-                default:
-                    throw new Error("[CheckParam] " + param + "のタイプが不正です: " + type);
-                    break;
-            }
-        }
-
-        return value;
-    };
-
-    const Params = {
-        "ActorStep" : CheckParam("num", "actor_step", 48, 0, 9999),
-    };
-
+    // const PN = "MKR_BattleCustomized_1";
 
     //=========================================================================
     // Sprite_Actor
     //  ・アクター前進処理を再定義します。
     //
     //=========================================================================
-    const _Sprite_Actor_stepForward = Sprite_Actor.prototype.stepForward;
-    Sprite_Actor.prototype.stepForward = function() {
-        _Sprite_Actor_stepForward.call(this);
-        this.startMove(-Params.ActorStep, 0, 12);
+    const _Sprite_Actor_updateTargetPosition = Sprite_Actor.prototype.updateTargetPosition;
+    Sprite_Actor.prototype.updateTargetPosition = function() {
+        _Sprite_Actor_updateTargetPosition.call(this);
+        if (this._actor.isInputting()) {
+            this.stepBack();
+        } else if(this._actor.isActing()) {
+            this.stepForward();
+        } else if (this._actor.canMove() && BattleManager.isEscaped()) {
+            this.retreat();
+        } else if (!this.inHomePosition()) {
+            this.stepBack();
+        }
     };
 
 
