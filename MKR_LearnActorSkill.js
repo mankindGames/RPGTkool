708b2aac3d025318a7c70667656e0d64c04e43e5
mankind <mkr.tk.games@gmail.com>
2018-04-01 16:03:40 +0900

    ver 1.0.1 : ステートの付与によりスキルを覚える機能を追加した。

--- MKR_LearnActorSkill.js
+++ MKR_LearnActorSkill.js
@@ -1,11 +1,13 @@
 //=============================================================================
 // MKR_LearnActorSkill.js
 //===============================================================================
-// Copyright (c) 2016-2017 マンカインド
+// Copyright (c) 2016-2018 マンカインド
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 // ------------------------------------------------------------------------------
 // Version
+// 1.0.1 2018/04/01 ステートの付与によりスキルを覚える機能を追加した。
+//
 // 1.0.0 2017/08/12 初版公開。
 // ------------------------------------------------------------------------------
 // [Twitter] https://twitter.com/mankind_games/
@@ -15,25 +17,27 @@
 
 /*:
  * ==============================================================================
- * @plugindesc (v1.0.0) アクター毎装備スキル
+ * @plugindesc (v1.0.1) アクター毎スキル習得
  * @author マンカインド
  *
- * @help = アクター毎装備スキル ver 1.0.0 =
+ * @help = アクター毎スキル習得 ver 1.0.0 =
  * MKR_LearnActorSkill.js - マンカインド
  *
- * 武器・防具のメモ欄に設定することで、装備したときに覚えるスキルを
- * アクター毎に変更することができます。
+ * 武器・防具/ステートのメモ欄に設定することで、
+ * アクターが装備/アクターにステート付与したときに覚える
+ * スキルをアクター毎に変更することができます。
  *
  *
- * 武器・防具メモ欄:
+ * 武器・防具/ステートメモ欄:
  *   <actorSkills_[アクターId]:[スキルID]>
- *     ・このメモが設定されたアイテムを[アクターID]番のアクターが装備した時、
+ *     ・このメモが設定されたアイテム/ステートを[アクターID]番の
+ *       アクターが装備/アクターに付与した時、
  *       [スキルID]に設定したスキルをそのアクターが使えるようになります。
  *
  *     ・[スキルID]はカンマ区切りで複数指定することもできます。
  *
- *     ・装備を外した場合、アクターは[スキルID]のスキルを使うことが
- *       できなくなります。
+ *     ・装備を外した/ステートが解除された場合、アクターは[スキルID]の
+ *       スキルを使うことができなくなります。
  *
  *     ・このメモを改行区切りで設定することで、複数のアクターそれぞれに
  *       スキルを設定することができます。
@@ -41,15 +45,18 @@
  *
  * メモ欄の設定例:
  *   <actorSkills_3:71>
- *     ・このアイテムをアクターID:3のアクターが装備した時、
+ *     ・このアイテム/ステートをアクターID:3の
+ *       アクターが装備/アクターに付与した時、
  *       そのアクターはスキルID:71のスキルを使えるようになります。
  *
  *   <actorSkills_1:1,3>
  *   <actorSkills_3:4>
- *     ・この装備をアクターID:1のアクターが装備した時、
+ *     ・このアイテム/ステートをアクターID:1の
+ *       アクターが装備/アクターに付与した時、
  *       そのアクターはスキルID:1と3のスキルを使えるようになります。
  *
- *     ・この装備をアクターID:3のアクターが装備した時、
+ *     ・このアイテム/ステートをアクターID:3の
+ *       アクターが装備/アクターに付与した時、
  *       そのアクターはスキルID:4のスキルを使えるようになります。
  *
  *
@@ -86,10 +93,6 @@
  *     ご了承ください。
  *
  * ==============================================================================
- *
- *
- *
- * ==============================================================================
 */
 
 var Imported = Imported || {};
@@ -98,7 +101,11 @@ Imported.MKR_learnActorSkill = true;
 (function () {
     'use strict';
 
-    // var PN = "MKR_LearnActorSkill";
+    // const PN = "MKR_LearnActorSkill";
+
+    const Regex = {
+        actorSkills : /<(?:actorskills_)(\d+)[ :]+([0-9, ]*)?>/i,
+    };
 
 
     //=========================================================================
@@ -106,36 +113,79 @@ Imported.MKR_learnActorSkill = true;
     //  ・アクターのスキルを再定義します。
     //
     //=========================================================================
-    var _Game_Actor_skills = Game_Actor.prototype.skills;
+    const _Game_Actor_skills = Game_Actor.prototype.skills;
     Game_Actor.prototype.skills = function() {
-        var ret, id, notes, pattern, m, skills, actorId;
+        let ret;
         ret = _Game_Actor_skills.call(this);
-        pattern = /<(?:actorskills_)(\d+)[ :]+([0-9, ]*)?>/i;
+        ret = this.learnEquipSkill(ret);
+        ret = this.learnStateSkill(ret);
+
+        return ret;
+    };
+
+    Game_Actor.prototype.learnEquipSkill = function(ret) {
+        let id, notes, pattern, m, skills, actorId;
         actorId = this.actorId();
 
         this.equips().forEach(function(item) {
-            if(item && item.note != "") {
-                notes = item.note.split(/ ?(?=<)/);
-                notes.forEach(function(note, i) {
-                    note = note.trim();
-                    if(pattern.test(note)) {
-                        m = note.match(pattern);
-                        m[1] = m[1].trim();
-                        m[2] = m[2].trim();
-                        id = isFinite(m[1]) ? parseInt(m[1], 10) : 0;
-                        skills = m[2].split(",").map(function(num) {
-                            return isFinite(num.trim()) ? parseInt(num.trim(), 10) : 0;
-                        });
-                        if(id == actorId) {
-                            skills.forEach(function(skill) {
-                                if(skill > 0 && $dataSkills[skill] && !ret.contains($dataSkills[skill])) {
-                                    ret.push($dataSkills[skill]);
-                                }
-                            });
-                        }
-                    }
-                });
+            if(!item || item.note == "") {
+                return false;
             }
+            notes = item.note.split(/ ?(?=<)/);
+            notes.forEach(function(note, i) {
+                note = note.trim();
+                if(!Regex.actorSkills.test(note)) {
+                    return false;
+                }
+                m = note.match(Regex.actorSkills);
+                m[1] = m[1].trim();
+                m[2] = m[2].trim();
+                id = isFinite(m[1]) ? parseInt(m[1], 10) : 0;
+                skills = m[2].split(",").map(function(num) {
+                    return isFinite(num.trim()) ? parseInt(num.trim(), 10) : 0;
+                });
+                if(id == actorId) {
+                    skills.forEach(function(skill) {
+                        if(skill > 0 && $dataSkills[skill] && !ret.contains($dataSkills[skill])) {
+                            ret.push($dataSkills[skill]);
+                        }
+                    });
+                }
+            });
+        });
+
+        return ret;
+    };
+
+    Game_Actor.prototype.learnStateSkill = function(ret) {
+        let id, notes, pattern, m, skills, actorId;
+        actorId = this.actorId();
+
+        this.states().forEach(function(item) {
+            if(!item || item.note == "") {
+                return false;
+            }
+            notes = item.note.split(/ ?(?=<)/);
+            notes.forEach(function(note, i) {
+                note = note.trim();
+                if(!Regex.actorSkills.test(note)) {
+                    return false;
+                }
+                m = note.match(Regex.actorSkills);
+                m[1] = m[1].trim();
+                m[2] = m[2].trim();
+                id = isFinite(m[1]) ? parseInt(m[1], 10) : 0;
+                skills = m[2].split(",").map(function(num) {
+                    return isFinite(num.trim()) ? parseInt(num.trim(), 10) : 0;
+                });
+                if(id == actorId) {
+                    skills.forEach(function(skill) {
+                        if(skill > 0 && $dataSkills[skill] && !ret.contains($dataSkills[skill])) {
+                            ret.push($dataSkills[skill]);
+                        }
+                    });
+                }
+            });
         });
 
         return ret;
